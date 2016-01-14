c34b90ded3c02f2999a9c7725a2bc2fe98c5ba4d
steffolino <stefan.stretz@stud.uni-bamberg.de>
2016-01-14 19:03:14 +0100

    part 2 aftermath
    disabled log on web pages

--- Rechnungstool/protected/config/main.php
+++ Rechnungstool/protected/config/main.php
@@ -159,10 +159,10 @@ return array(
 					'levels'=>'error, warning',
 				),
 				// uncomment the following to show log messages on web pages
-				array(
-					'class'=>'CWebLogRoute',
-					'levels'=>'error, warning,trace,info,debug',
-				),
+				// array(
+					// 'class'=>'CWebLogRoute',
+					// 'levels'=>'error, warning,trace,info,debug',
+				// ),
 				
 				
 			),
